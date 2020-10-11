b0f3aee3dd17787757b895f9163be86e091d904a
Derion23 <kevin_klug@gmx.de>
2020-10-11 17:31:27 +0200

    Updates

---
+++ precache-manifest.acba6015df87cd18e248590591d3f95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a355293e1bcf55e650cdb0807c335344",
+    "url": "/insuline-calculator/index.html"
+  },
+  {
+    "revision": "2493e90a96acd44b63d8",
+    "url": "/insuline-calculator/static/js/2.4059f625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insuline-calculator/static/js/2.4059f625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33014186a6511cfbfa56",
+    "url": "/insuline-calculator/static/js/main.201be28f.chunk.js"
+  },
+  {
+    "revision": "8cf65afbf204839c20cc",
+    "url": "/insuline-calculator/static/js/runtime-main.34e80a8b.js"
+  }
+]);
