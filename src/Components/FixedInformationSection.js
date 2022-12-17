61af4f705880bec4822cc0e172337b880faeca37
Derion23 <kevin_klug@gmx.de>
2022-12-17 18:39:36 +0100

    changed automatic description to auto

--- src/Components/FixedInformationSection.js
+++ src/Components/FixedInformationSection.js
@@ -36,7 +36,7 @@ export default function FixedInformationSection({
             setDayTimeChoice('automatic')
             refreshPage()
           }}>
-          Automatic
+          Auto
         </button>
 
         <button
